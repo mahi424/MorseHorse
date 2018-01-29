42e51e75c114a16058f9df7485c34f08e87b113c
mahi424 <mahaveerchouhan424@gmail.com>
2018-01-29 19:19:10 +0530

    T2Mnew

--- MorseHorse.py
+++ MorseHorse.py
@@ -69,94 +69,35 @@ choice = choicebox(msg, title, choices)
 if choice=="Morse Code To Text":
 	print "yes"
 else:
-	# print "no"
 	msg ="How Do You Like It?"
 	title = "MorseHorse"
 	choices = ["From console to console","From console to text file","From text file to console","From text file to text file" ]
 	choice = choicebox(msg, title, choices)
-	# print choice
-
+	
 	#################################################################################################################
 	# from console to console
 	if choice=="From console to console":
 		str_input=fromconsole()
-
-		# msg ="Enter You Text"
-		# title = "MorseHorse"
-		# str_input = enterbox(msg, title)
 		result=MorseCodeGen(str_input)
 		toconsole(result)
-		# result = ''
-		# for char in str_input:
-		# 	if str(char).isalnum():
-		# 		result += MorseCode[char.upper()]+'   '
-		# 	elif c==' ':
-		# 		result += '       '
-		# print result
 	#################################################################################################################
 	# from console to file
 	elif choice=="From console to text file":
-		# msg ="Enter You Text"
-		# title = "MorseHorse"
-		# str_input = enterbox(msg, title)
 		str_input=fromconsole()
 		result=MorseCodeGen(str_input)
 		tofile(result)
-
-
-		# f=open('Result.txt','w')
-
-		# result = ''
-		# for char in str_input:
-		# 	if str(char).isalnum():
-		# 		result += MorseCode[char.upper()]+'   '
-		# 	elif c==' ':
-		# 		result += '       '
-		# f.write(result)
-		# f.close()
-		# msg ="Your Result written to Result.txt"
-		# title = "MorseHorse"
-		# msgbox(msg,title, ok_button="Good job!")
 	#################################################################################################################
 	# from file to console
 	elif choice=="From text file to console":
 		str_input=fromfile()
 		result=MorseCodeGen(str_input)
 		toconsole(result)
-		# path=easygui.fileopenbox();
-		# f=open(path,'r')
-		# data=f.read()
-		# data = data.rstrip("\n\r")
-		# result = ''
-		# for char in data:
-		# 	if str(char).isalnum():
-		# 		result += MorseCode[char.upper()]+'   '
-		# 	elif char==' ':
-		# 		result += '       '
-		# print result
 	#################################################################################################################
 	# from file to file
 	elif choice=="From text file to text file":
 		str_input=fromfile()
 		result=MorseCodeGen(str_input)
 		tofile(result)
-		# path=easygui.fileopenbox();
-		# f=open(path,'r')
-		# str_input=f.read()
-		# str_input = str_input.rstrip("\n\r")
-		# f=open('Result.txt','w')
-
-		# result = ''
-		# for char in str_input:
-		# 	if str(char).isalnum():
-		# 		result += MorseCode[char.upper()]+'   '
-		# 	elif c==' ':
-		# 		result += '       '
-		# f.write(result)
-		# f.close()
-		# msg ="Your Result written to Result.txt"
-		# title = "MorseHorse"
-		# msgbox(msg,title, ok_button="Good job!")
 	else:
 		print "Error!!!"
 
