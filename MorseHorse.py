6fc2c8ca899712f85f60979ac5c371c4fd1da96a
mahi424 <mahaveerchouhan424@gmail.com>
2018-01-30 13:14:22 +0530

    Functional

--- MorseHorse.py
+++ MorseHorse.py
@@ -2,7 +2,8 @@
 import easygui
 from easygui import *
 
-MorseCode= { 'A':'.-', 'B':'-...',
+MorseCode= { 
+					'A':".-", 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
                     'I':'..', 'J':'.---', 'K':'-.-',
@@ -15,10 +16,14 @@ MorseCode= { 'A':'.-', 'B':'-...',
                     '4':'....-', '5':'.....', '6':'-....',
                     '7':'--...', '8':'---..', '9':'----.',
                     '0':'-----',
+                    # 'AB':'ttt',
                     # ', ':'--..--', '.':'.-.-.-',
                     # '?':'..--..', '/':'-..-.', '-':'-....-',
                     # '(':'-.--.', ')':'-.--.-'
                     }
+
+
+# inv_map={'---': 'O', '--.': 'G', '-...': 'B', '-..-': 'X', '.-.': 'R', '--.-': 'Q', '--..': 'Z', '.--': 'W', '..---': '2', '.-': 'A', '..': 'I', '-.-.': 'C', '..-.': 'F', '-.--': 'Y', '-': 'T', '.': 'E', '.-..': 'L', '...': 'S', '..-': 'U', '.----': '1', '-----': '0', '-.-': 'K', '-..': 'D', '----.': '9', '-....': '6', '.---': 'J', '.--.': 'P', '....-': '4', '--': 'M', '-.': 'N', '....': 'H', '---..': '8', '...-': 'V', '--...': '7', '.....': '5', '...--': '3'}
 
 def MorseCodeGen(data):
 	result = ''
@@ -30,11 +35,57 @@ def MorseCodeGen(data):
 	# print result
 	return result
 
+def MorseCodeDec(data,inv_map):
+	print 'Called'
+	count=0;
+	# data='.-'
+	result = ''
+	temp= ''
+	# temp=temp+data
+	# print data
+	data=data+'   '
+	for char in data:
+		if char=='.' or char =='-':
+			temp=temp+char
+			# print temp
+			count=0
+		else:
+			count=count+1
+			if count==3:
+				result=result+inv_map[str(temp)]
+				# print inv_map[str(temp)]
+				temp=''
+			elif count==7:
+				result=result+' '
+
+	return result
+			# print count
+		# elif char==' ':
+		# 	result=result+inv_map[str(temp)]
+		# 	print result
+		# 	temp=''
+
+			# count+=1;
+			# print count
+			# if count==1:
+			# 	print temp
+			# 	result=result+inv_map[str(temp)]
+			# 	temp=''
+			# elif count==7:
+			# 	result=result+' '
+		# else:
+		# 	result=result+inv_map[temp]
+	# result=result+' '+'hi'
+	# print result
+			
+
+
 def fromconsole():
 	msg ="Enter You Text"
 	title = "MorseHorse"
 	str_input = enterbox(msg, title)
-	str_input = str_input.rstrip("\n\r")
+	# str_input = str_input.rstrip("\n\r")
+	print 'recieved'+str_input
 	return str_input
 
 
@@ -61,16 +112,53 @@ def tofile(result):
 
 
 
+
+
 msg ="What Can I Do For You?"
 title = "MorseHorse"
 choices = ["Morse Code To Text","Text To Morse Code"]
 choice = choicebox(msg, title, choices)
 
 if choice=="Morse Code To Text":
-	print "yes"
+	# print len(MorseCode)
+	inv_map = {v: k for k, v in MorseCode.iteritems()}
+	# print inv_map
+	msg ="How Do You Like It?"
+	title = "MorseHorse : Morse Code To Plain Text Conversion Mode"
+	choices = ["From console to console","From console to text file","From text file to console","From text file to text file" ]
+	choice = choicebox(msg, title, choices)
+	if choice=="From console to console":
+		str_input=fromconsole()
+		result=MorseCodeDec(str_input,inv_map)
+		toconsole(result)
+
+	# print inv_map['-...']
+	# print len(inv_map)
+	# print str(inv_map)
+	# print "yes"
+
+	elif choice=="From console to text file":
+		str_input=fromconsole()
+		result=MorseCodeDec(str_input)
+		tofile(result)
+	#################################################################################################################
+	# from file to console
+	elif choice=="From text file to console":
+		str_input=fromfile()
+		result=MorseCodeDec(str_input)
+		toconsole(result)
+	#################################################################################################################
+	# from file to file
+	elif choice=="From text file to text file":
+		str_input=fromfile()
+		result=MorseCodeDec(str_input)
+		tofile(result)
+	else:
+		print "Error!!!"
+
 else:
 	msg ="How Do You Like It?"
-	title = "MorseHorse"
+	title = "MorseHorse : Plain Text To Morse Code Conversion Mode"
 	choices = ["From console to console","From console to text file","From text file to console","From text file to text file" ]
 	choice = choicebox(msg, title, choices)
 	
